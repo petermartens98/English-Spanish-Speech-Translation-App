860daac7f74f78b3943a91ca03189a0ddf666427
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2022-10-14 20:53:37 -0400

    Update English_to_Spanish_V2.py

--- English_to_Spanish_V2.py
+++ English_to_Spanish_V2.py
@@ -54,7 +54,6 @@ female_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TT
 male_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
 engine.setProperty('voice', male_voice_id)
 
-
 # Ask user if they would like program to recite text in spanish
 print("")
 recite = input("Recite spanish translation (Y/N)?")
